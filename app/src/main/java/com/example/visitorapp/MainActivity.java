dab677ce8513d04825de79311ce78d4724271760
ankit2774 <ankitsamgeorge@gmail.com>
2024-12-19 09:20:32 +0530

    Visitor

---
+++ app/src/main/java/com/example/visitorapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.visitorapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button bT1,bT2;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        bT1=(Button) findViewById(R.id.b1);
+        bT2=(Button) findViewById(R.id.b2);
+
+
+        bT1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(getApplicationContext(),AddActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
